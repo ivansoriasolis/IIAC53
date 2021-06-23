8e164702a767ca48b710c0d9c97c1f594abd4ab2
ivansoriasolis <soria.solis.ivan@googlemail.com>
2021-06-22 19:11:31 -0500

    Update Ejemplo 10-1.php

--- 10/Ejemplo 10-1.php
+++ 10/Ejemplo 10-1.php
@@ -5,11 +5,18 @@
         echo "Bienvenido Usuario: " .  
              htmlspecialchars($_SERVER['PHP_AUTH_USER']).
              "Password: " . htmlspecialchars($_SERVER['PHP_AUTH_PW']);
+        echo '<form method="POST" action="Ejemplo 10-1.php">
+             <input type="submit" name="logout" value="Logout"></form>';
+        
+        if (isset($_POST['logout']))
+        {   header('WWW-Authenticate: Basic realm="protected area"');
+            header('HTTP/1.0 401 Unauthorized');
+            die("Saliste"); 
+        }           
     } 
     else
     {
         header('WWW-Authenticate: Basic realm="protected area"');
-        header('HTTP/1.0 401 Unauthorized');
         die("Please enter your username and password");    
     }
 ?>
